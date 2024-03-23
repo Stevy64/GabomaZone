39f5ab3ecff5668718ff733933593090b34abb9b
Steevy Boubala <boubalastevy64@gmail.com>
2024-03-23 01:06:19 +0100

    changing secret in sttings.py

--- gabomazone-app/project/settings.py
+++ gabomazone-app/project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%eckd70i##mznm!a55p^yb&3unlw3!r)d3_g*=1zrx$_*_hy^5'
+SECRET_KEY = 'django-insecure-mysecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
